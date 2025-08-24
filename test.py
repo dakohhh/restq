ebf1bb76f995369a5643e9dcd656d4e7f3091d31
dakohhh <wiizzydreadmill@gmail.com>
2025-08-24 01:56:15 +0100

    chore: added sync and async support for queue

--- test.py
+++ test.py
@@ -1,24 +1,18 @@
 import asyncio
 from typing import Any
-from restq import Queue
+from restq import AsyncQueue, Queue
 from test_pickle import TestPickleClass
-from redis.asyncio import Redis as AsyncRedis, from_url as redis_from_url
+from redis import Redis, from_url as redis_from_url
 
 
 REDIS_URL = "redis://localhost:6379/0"
 
-async def get_redis_client(*args: Any, **kwargs: Any) -> AsyncRedis: # type: ignore
-    redis = await redis_from_url(REDIS_URL, *args, **kwargs)
-
-    return redis
-
 
 async def main() -> None:
-    redis = await get_redis_client(decode_responses=True)
 
-    queue = Queue(name="email-queue", redis=redis)
+    queue = Queue(name="email-queue", redis=redis_from_url(url=REDIS_URL, decode_responses=True)) # type: ignore
 
-    await queue.add(task_name="sendWorkEmailTask", delay=100, kwargs={"name": "Wisdom"}, mode="json")
+    queue.add(task_name="sendWorkEmailTask", kwargs={"name": "Wisdom"}, mode="json")
 
 
 
--- worker.py
+++ worker.py
@@ -7,23 +7,23 @@ from redis.asyncio import Redis as AsyncRedis, from_url as redis_from_url
 
 REDIS_URL = "redis://localhost:6379/0"
 
-async def get_redis_client(*args: Any, **kwargs: Any) -> AsyncRedis: # type: ignore
-    redis = await redis_from_url(REDIS_URL, *args, **kwargs)
+# async def get_redis_client(*args: Any, **kwargs: Any) -> AsyncRedis: # type: ignore
+#     redis = await redis_from_url(REDIS_URL, *args, **kwargs)
 
-    return redis
+#     return redis
 
 @task(name="sendWorkEmailTask")
-def send_work_email(name: str) -> None:
-    print("Started something task")
+async def send_work_email(name: str) -> None:
+    print("Started something task async")
     print(f"Sending.... work email to {name}")
     print("Done")
 
 
 
 async def main() -> None:
-    redis = await get_redis_client(decode_responses=True)
+    # redis = await get_redis_client(decode_responses=True)
 
-    worker = Worker(queue_name="email-queue", redis=redis, tasks=[send_work_email])
+    worker = Worker(queue_name="email-queue", url=REDIS_URL, tasks=[send_work_email])
 
     await worker.start()
 
